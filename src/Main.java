0921e043e4e8c1e760c492ef3faf351ac907e4f0
Alyona Kudryavtseva <sunny_aga@mail.ru>
2022-01-10 11:42:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+
+            int ticketPrice = 5000;
+            int mile = ticketPrice / 20;
+            System.out.println("Вам начислено:" + mile + " миль.");
+        }
+
+    }
